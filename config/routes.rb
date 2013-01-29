22b389f3efc13a080ae48f1c83a60abf2fa35470
Muhammad Imran Malik <imran@nxb.com.pk>
2013-01-29 14:42:31 +0500

    route root to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -123,7 +123,7 @@ Osb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect("/payments")
+  root :to => redirect("/dashboard")
 
   # See how all your routes lay out with "rake routes"
 
