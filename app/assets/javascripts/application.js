d4bab245e04e69b7ced6e6d1a0e58b3d89d2f5cc
Muhammad Imran Malik <imran@nxb.com.pk>
2013-02-01 21:23:49 +0500

    assets manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,11 @@
 //= require jquery-ui.js
 //= require twitter/bootstrap
 //= require_tree .
+//= require jquery_nested_form
+//= require nav
+//= require chosen.jquery
+//= require jquery.css3caching.js
+//= require invoices.js.coffee
 
 jQuery(function(){
     //override default behavior of inserting new subforms into form    
