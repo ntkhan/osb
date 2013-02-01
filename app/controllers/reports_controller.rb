812b77bd32a6308a03d6ec43561ed566138e3fe6
Muhammad Imran Malik <imran@nxb.com.pk>
2013-02-01 11:48:14 +0500

    Paymetns collected in progress

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,5 +8,7 @@ class ReportsController < ApplicationController
   def payments_collected
     criteria = params
     @payments_collected = Reports.payments_collected(criteria)
+    @payments_total = @payments_collected .reject{|payment| payment[:payment_method] == "check"}
+                                          .inject(0){|total, payment| total + payment[:payment_amount]}
   end
 end
