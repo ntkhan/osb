d07b42b5618b8c2153b8b8ca4a7ea772df35b81b
arif <arif@arif-desktop.(none)>
2013-02-04 20:25:25 +0500

    popover messages

--- app/views/payments/bulk_actions.js
+++ app/views/payments/bulk_actions.js
@@ -1,12 +1,21 @@
 jQuery(".alert").hide()
 <%if params[:payment_ids].blank? %>
-jQuery(".alert.alert-error").show().find('span').html("You haven't selected any payment to delete/archive. Please select one or more payments and try again.");
+<%
+  if @action == "recovered from archived" or @action == "recovered from deleted"
+    no_msg = "Recover"
+  else
+    no_msg = "delete/archive"
+  end
+%>
+jQuery(".alert.alert-error").show().find('span').html("You haven't selected any payment to <%= no_msg%>. Please select one or more payments and try again.");
 <% elsif @action == "archived" or @action == "deleted" %>
 jQuery(".alert.alert-success").show().find('span').html("<%= escape_javascript @message %>");
 <% else %>
 jQuery(".alert.alert-success").show().find('span').html("Payment(s) are <%= @action %> successfully");
 <% end %>
+<%unless params[:payment_ids].blank? %>
 jQuery('tbody#payment_body').html('<%= escape_javascript render("payment") %>');
+<%end%>
 jQuery('#active_links').html('<%= escape_javascript render("filter_links") %>');
 jQuery('#active_links a').removeClass('active');
 <% if @action == "recovered from archived"%>
