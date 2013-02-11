992ee12dc9afacda747fd653ffcfdcf9ee7fcab9
Muhammad Imran Malik <imran@nxb.com.pk>
2013-02-11 17:41:00 +0500

    Page like look and reports

--- lib/reporting/criteria.rb
+++ lib/reporting/criteria.rb
@@ -1,16 +1,21 @@
 module Reporting
   class Criteria
 
-    attr_accessor :from_date, :to_date, :client_id, :payment_method, :item_id
+    # criteria attributes for *Payments Collected* report
+    attr_accessor :from_date, :to_date, :client_id, :payment_method
+
+    # attributes for *Revenue by Clients* report
+    attr_accessor :year
 
     def initialize(options={})
       Rails.logger.debug "--> Criteria init... #{options.to_yaml}"
       options ||= {} # if explicitly nil is passed then convert it to empty hash
-      @from_date = (options[:from_date] || 1.month.ago).to_date
-      @to_date = (options[:to_date] || Date.today).to_date
-      @client_id = (options[:client_id] || 0).to_i
-      @payment_method = (options[:payment_method] || "")
-      @item_id = (options[:item_id] || 0).to_i
+      @from_date = (options[:from_date] || 1.month.ago).to_date # default for one month back from today
+      @to_date = (options[:to_date] || Date.today).to_date # default to today
+      @client_id = (options[:client_id] || 0).to_i # default to all i.e. 0
+      @payment_method = (options[:payment_method] || "") # default to all i.e. ""
+
+      @year = (options[:year] || Date.today.year).to_i # default to current year
       Rails.logger.debug "--> Criteria init... #{self.to_yaml}"
     end
   end
