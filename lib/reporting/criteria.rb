437fda62be1b5b026c283af7d62227b1fd7eb998
Muhammad Imran Malik <imran@nxb.com.pk>
2013-02-11 12:35:25 +0500

    Payments collected report done

--- lib/reporting/criteria.rb
+++ lib/reporting/criteria.rb
@@ -8,9 +8,9 @@ module Reporting
       options ||= {} # if explicitly nil is passed then convert it to empty hash
       @from_date = (options[:from_date] || 1.month.ago).to_date
       @to_date = (options[:to_date] || Date.today).to_date
-      @client_id = options[:client_id] || 0
-      @payment_method = options[:payment_method] || 0
-      @item_id = options[:item_id] || 0
+      @client_id = (options[:client_id] || 0).to_i
+      @payment_method = (options[:payment_method] || "")
+      @item_id = (options[:item_id] || 0).to_i
       Rails.logger.debug "--> Criteria init... #{self.to_yaml}"
     end
   end
