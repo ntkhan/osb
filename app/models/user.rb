eed9f4934368cbf5f9e783d9427ab8a91e538fbe
Arif Khan <arif@NX00151.(none)>
2013-02-13 11:15:45 +0500

    company users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  belongs_to :company
+  #has_many :company, :through => :company_users
+  has_and_belongs_to_many :companies, :join_table => "company_users"
 
   def currency_symbol
     # self.company.currency_symbol
