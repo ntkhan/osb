5fd0fdac54fe7c2ac4e6944306251a4cafbd3ce8
Arif Khan <arif@NX00151.(none)>
2013-02-14 16:05:41 +0500

    send emails

--- app/views/sent_emails/index.js
+++ app/views/sent_emails/index.js
@@ -1,3 +1,4 @@
 jQuery('tbody#email_body').html('<%= escape_javascript render("sent_emails") %>');
 jQuery('.paginator').html('<%= escape_javascript(paginate(@sent_emails, :remote => true).to_s) %>' +
     '<div class="paging_info"><%= page_entries_info @sent_emails %></div>');
+jQuery("table.table_listing").trigger('update');
